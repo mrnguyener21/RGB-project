81d9dac4a16f3d868ae726c8a303b201102afe4f
Victor Nguyen <victortnguyen.workemail@gmail.comm>
2020-02-10 22:41:24 -0800

    updated HTML & CSS with the correct div. Updated step 2. Started on rough draft of step6

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-//STEPS//
-// 1 connect all of the html classes to a dom//
-//2  create a function that generates three random numbers from 0-255 and put them in variables called r g and b. that function nees to return a string '(rgb,(r,g,b)' letter being dynamic numbers)
-//3 create a function that returns the color array. that array contains 6 colors. each color is a string of the format mentioned above.
-//4 create the event for the "easy button" to hide or maybe even comment out the bottom row
-//5 create the event for the "hard button" to reveal the bottom row 
-//6 create the event for the "new color button" to change the colors for the squares
-//7 create event listeners for the squares so that when you click on one you they disappear 
-
 //STEP 1CONNECT THE CLASSES AND ID TO DOM
 //the top section//
 let theGreat = document.querySelector(".theGreat");
@@ -36,24 +27,30 @@ let color5 = document.querySelector("#color5");
 let color6 = document.querySelector("#color6");
 
 //STEP 2 Create a function that generates three random numbers from 0 - 255 and put them in variables r,g,b. 
-red.innerHTML = Math.floor(Math.random()*255)+1;
-green.innerHTML = Math.floor(Math.random()*255)+1;
-blue.innerHTML = Math.floor(Math.random()*255)+1;
+let RGBGenerator = () => {
+    red.innerHTML = Math.floor(Math.random()*255)+1;
+    green.innerHTML = Math.floor(Math.random()*255)+1;
+    blue.innerHTML = Math.floor(Math.random()*255)+1;
+    return red,green,blue;
+}
 
 // STEP 3 create a function that returns the color array. that array contains 6 colors. each color is a string of the format mentioned above.
 
 
 //4 create the event for the "easy button" to hide or maybe even comment out the bottom row
-let theEasyButton = easy.addEventListener("click",function(){
-        element.classList.remove("bottomRow")
-    });
+let theEasyButton = easy.addEventListener("click", () => element.classList.remove("bottomRow"));
 
 //5 create the event for the "hard button" to reveal the bottom row 
-let theHardButton = hard.addEventListener("click",function(){
-    element.classList.add("bottomRow")
-});
+let theHardButton = hard.addEventListener("click", () => element.classList.add("bottomRow"));
 
-//6 create the event for the "new color button" to change the colors for the squares
+//6 create event listeners for the squares so that when you click on one you they disappear. create if else statement to where if inner html of the square matches RGB or not to display wrong or correct 
+if (color !== RGBGenerator){
+    element.classlist.add("tryAgain");
+} else {
+    element.classlist.remove("tryAgain");
+    element.classList.add("correct");
+    //probbaly gonna have ot remove the event listeners from the colors so the game can't continue unless you restart by clicking one of the buttons
+}
 
 
-//7 create event listeners for the squares so that when you click on one you they disappear 
+//7 create the event for the "new color button" to change the colors for the squares and to add back the squares that been removed
