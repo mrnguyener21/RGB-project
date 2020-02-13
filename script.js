6eb5b9a7f0b54d0109c85542f74f00cf48ea495f
Victor Nguyen <victortnguyen.workemail@gmail.comm>
2020-02-12 18:17:38 -0800

    fixed javascript along with eplementing the next few steps

--- script.js
+++ script.js
@@ -1,62 +1,46 @@
-//STEP 1CONNECT THE CLASSES AND ID TO DOM
-//the top section//
-let theGreat = document.querySelector(".theGreat");
-let RGBColor = document.querySelector(".RGBColor");
-let red = document.getElementById("red");
-let blue = document.getElementById("green");
-let green = document.getElementById("blue");
-let colorGame = document.querySelectory(".colorGame");
+const modeButtons = document.querySelectorAll('.mode');
+const resetButton = document.querySelector('#reset');
+const squares = document.querySelectorAll(".square");
+const title = document.querySelector("#title");
+const colorDisplay = document.querySelector("#colorDisplay");
+const messageDisplay = document.querySelector("#messageDisplay");
 
-//the button sections
-let options = document.querySelector(".options");
-let button = document.querySelector(".button");
-let level = document.querySelector(".level");
-let easy = document.querySelector("#easy");
-let hard = document.querySelector("#hard");
+let colors = [];
+let pickedColor = null;
 
-//the square sections
-let theColors = document.querySelector(".theColors");
-let topRow = document.querySelector(".topRow");
-let bottomRow = document.querySelector(".bottomRow");
-let square = document.querySelector(".square");
-let square1 = document.querySelector("#color1");
-let square2 = document.querySelector("#color2");
-let square3 = document.querySelector("#color3");
-let square4 = document.querySelector("#color4");
-let square5 = document.querySelector("#color5");
-let square6 = document.querySelector("#color6");
+const generateColor = () => {
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
 
-//STEP 2 Create a function that generates three random numbers from 0 - 255 and put them in variables r,g,b. That function needs to return a string 'rgb(r, g, b)', letter being dynamic numbers
-let RGBGenerator = () => {
-    let r = toString(red.innerHTML = Math.floor(Math.random()*255)+1);
-    let g = toString(green.innerHTML = Math.floor(Math.random()*255)+1);
-    let b = toString(blue.innerHTML = Math.floor(Math.random()*255)+1);
-
-    return 'rgb(' + r + ',' + g + ',' + b + ')';
+    return `rgb(${r}, ${g}, ${b})`;
 }
 
-// STEP 3 create a function that returns the square array. that array contains 6 colors. each square is a string of the format mentioned above. this part should be the one to change the colors of the square
-let colorArray = [square1,square2,square3,square4,square5,square6];
-//maybe i can assign a randomized math formula to each of the array's index while creating a formula to randomize the order of the array. so that way ex: index 1 will always match to the RGBGenerator, but it will belong to a different squre each time since the order will change everytime 
-randomColor1 = colorArray[1].Math.floor(Math.random()*255)+1;
-randomColor2 = colorArray[2].Math.floor(Math.random()*255)+1;
-randomColor3 = colorArray[3].Math.floor(Math.random()*255)+1;
-randomColor4 = colorArray[4].Math.floor(Math.random()*255)+1;
-randomColor5 = colorArray[5].Math.floor(Math.random()*255)+1;
-correctColor = colorAray[6].RGBGenerator;
-//4 create the event for the "easy button" to hide or maybe even comment out the bottom row
-let theEasyButton = easy.addEventListener("click", () => element.classList.remove("bottomRow"));//this probably didn't work because the rest square divs still exist
+// call generateColors with a paramter called number
+const generateColors = () => {
+    // based on the number, dynamically add either 3 or 6 colors
+    for (let i = 0; i < 6; i++) {  
+        const color = generateColor();
 
-//5 create the event for the "hard button" to reveal the bottom row 
-let theHardButton = hard.addEventListener("click", () => element.classList.add("bottomRow"));
+        colors.push(color);
+    }
 
-//6 create event listeners for the squares so that when you click on one you they disappear. create if else statement to where if inner html of the square matches RGB or not to display wrong or correct 
-if (square !== RGBGenerator){
-    element.classlist.add("tryAgain");
-} else {
-    element.classlist.remove("tryAgain");
-    element.classList.add("correct");
-    //probbaly gonna have ot remove the event listeners from the colors so the game can't continue unless you restart by clicking one of the buttons
-};
+    // dynamically pick either one of the 3 colors, or one of the 6 colors
+    pickedColor = colors[Math.floor(Math.random() * 5)];
+}
 
-//7 create the event for the "new square button" to change the colors for the squares and to add back the squares that been removed
+generateColors();
+
+// call generateColors with a param which is either 3 or 6
+
+// create a function called setupSquares
+// loop through all the squares
+// add event listeners to all the squares
+// change the background of each square to one of the colors in the colors array
+
+// change the colorDisplay to pickedColor
+
+// add event listeners to modeButtons
+// add event listener to resetButton 
+
+// AS ALWAYS, GOOD LUCK! :D
