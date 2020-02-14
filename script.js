1dbd7c5eb4ca84a10188c1a40062c307b0f401cf
Victor Nguyen <victortnguyen.workemail@gmail.comm>
2020-02-14 07:39:02 -0800

    progress on squares and mode button

--- script.js
+++ script.js
@@ -5,8 +5,9 @@ const title = document.querySelector("#title");
 const colorDisplay = document.querySelector("#colorDisplay");
 const messageDisplay = document.querySelector("#messageDisplay");
 
+console.log(modeButtons);
 let colors = [];
-let pickedColor = null;
+let pickedColor = null;//what was the point of this line again lol
 
 const generateColor = () => {
     const r = Math.floor(Math.random() * 255);
@@ -17,30 +18,40 @@ const generateColor = () => {
 }
 
 // call generateColors with a paramter called number
+
+
 const generateColors = () => {
     // based on the number, dynamically add either 3 or 6 colors
     for (let i = 0; i < 6; i++) {  
         const color = generateColor();
-
         colors.push(color);
-    }
-
+    };
+    
     // dynamically pick either one of the 3 colors, or one of the 6 colors
     pickedColor = colors[Math.floor(Math.random() * 5)];
 }
 
-generateColors();
-
 // call generateColors with a param which is either 3 or 6
+generateColors(3,6);
 
 // create a function called setupSquares
+const setupSquares = ()=> {
 // loop through all the squares
+    for (let i = 0; i < 6; i++) {
+        squares[i];
+    }
+};
 // add event listeners to all the squares
+setupSquares.addEventListener("click", () =>// need a function to compare it to "pickedColor" to see if it is right or not. if wrong disapear show try again label );
 // change the background of each square to one of the colors in the colors array
+
 
 // change the colorDisplay to pickedColor
 
+
 // add event listeners to modeButtons
+modeButtons.addEventListener("click", () => modeButtons[0]? /* gotta connect this to generateColors(3)*/ : /*gotta connect this to generateColors(6)*/);
 // add event listener to resetButton 
+resetButton.addEventListener("click", () =>);
 
 // AS ALWAYS, GOOD LUCK! :D
