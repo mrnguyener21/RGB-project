596d928c0d5493a166d41cda3dabbab9d34cebe9
Victor Nguyen <victortnguyen.workemail@gmail.comm>
2020-02-17 17:18:37 -0800

    updated so squares will disapear when clicked and user is notified if clicked square is correct or incorrect. Reset button is fixed to change to try again if correct square is clicked

--- script.js
+++ script.js
@@ -5,53 +5,83 @@ const title = document.querySelector("#title");
 const colorDisplay = document.querySelector("#colorDisplay");
 const messageDisplay = document.querySelector("#messageDisplay");
 
-console.log(modeButtons);
 let colors = [];
-let pickedColor = null;//what was the point of this line again lol
+let pickedColor = null;
+let numberOfSquares = 6;
+
+message.style.color = '#ffffff'
 
 const generateColor = () => {
     const r = Math.floor(Math.random() * 255);
     const g = Math.floor(Math.random() * 255);
     const b = Math.floor(Math.random() * 255);
-
+    
     return `rgb(${r}, ${g}, ${b})`;
 }
 
-// call generateColors with a paramter called number
+const generateColors = (number) => {
+    const generatedColors = [];
 
-
-const generateColors = () => {
-    // based on the number, dynamically add either 3 or 6 colors
-    for (let i = 0; i < 6; i++) {  
+    for (let i = 0; i < number; i++) {  
         const color = generateColor();
-        colors.push(color);
+        
+        generatedColors.push(color);
     };
     
-    // dynamically pick either one of the 3 colors, or one of the 6 colors
-    pickedColor = colors[Math.floor(Math.random() * 5)];
+    return generatedColors;
+}
+generateColors();
+
+
+
+const pickColorToGuess = () => colors[Math.floor(Math.random() * numberOfSquares)];
+pickColorToGuess();
+
+
+const reset = () => {
+    colors = generateColors(numberOfSquares);
+    pickedColor = pickColorToGuess();
+
+    for (let i = 0; i < numberOfSquares; i++) {
+        if(colors[i]){
+            squares[i].style.display = "block";
+            squares[i].style.background = colors[i];
+        } else {
+            squares[i].style.display = "none";
+        }    
+    }
 }
 
-// call generateColors with a param which is either 3 or 6
-generateColors(3,6);
+reset();
 
-// create a function called setupSquares
-const setupSquares = ()=> {
-// loop through all the squares
-    for (let i = 0; i < 6; i++) {
-        squares[i];
-    }
-};
-// add event listeners to all the squares
-setupSquares.addEventListener("click", () =>// need a function to compare it to "pickedColor" to see if it is right or not. if wrong disapear show try again label );
-// change the background of each square to one of the colors in the colors array
+for (let i = 0; i < numberOfSquares; i++) {        
+    squares[i].addEventListener("click", () => {
+        const clickedColor = squares[i].style.background;
 
-
-// change the colorDisplay to pickedColor
-
+        if (clickedColor === pickedColor){
+            // change the messageDisplay to 'Correct!' (use textContent)
+            message.textContent = 'Correct!'
+            // change the resetButton to 'Play Again?' (use textContent)
+            resetButton.textContent = 'Play Again?'
+            // change the color of the title to pickedColor
+            message.style.background = pickedColor;
+            if (pickedColor = `${255}, ${255}, ${255})`){ 
+                message.style.color = '#000000';//need help with this
+            };
+            //make all the square the show the same color(the correct color)
+            squares[i].style.background = pickedColor;
+            //make it so you can't continue the game...need to elaborate   
+        }else { 
+            // change the messageDisplay to 'Try Again!' (use textContent)
+            squares[i].style.background = '#232323';
+            message.textContent = 'Try Again!';
+            message.style.background = '#ff0000';
+        }
+    });
+}
 
 // add event listeners to modeButtons
-modeButtons.addEventListener("click", () => modeButtons[0]? /* gotta connect this to generateColors(3)*/ : /*gotta connect this to generateColors(6)*/);
+// modeButtons.addEventListener("click", () => modeButtons[0]? /* gotta connect this to generateColors(3)*/ : /*gotta connect this to generateColors(6)*/);
 // add event listener to resetButton 
-resetButton.addEventListener("click", () =>);
-
-// AS ALWAYS, GOOD LUCK! :D
+// resetButton.addEventListener("click", () =>/* create a function to reset the colors, match to generateColors(6), and to match to pickedColors*/);
+``
